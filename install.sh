390bf1e572504083f2fa9f013fff8b41b289048f
Joseph Amigo <joseph.amigo@yahoo.com>
2025-11-14 18:14:17 -0500

    Resolving container mounting issues when no ownership.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 mkdir logs
+ssh -o StrictHostKeyChecking=accept-new burst "cd ~/rob6323_go2_project && sbatch --job-name='rob6323_${USER}' --mail-user='${USER}@nyu.edu' install.slurm '$@'"
 cd /scratch/$USER
 git clone https://github.com/isaac-sim/IsaacLab.git
